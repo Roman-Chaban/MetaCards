fe8f1ebb2fa9fae499c17a5c37be1fe0343ea03d
Roman Chaban <roman.chaban1001@gmail.com>
2025-05-24 01:51:24 +0300

    Add NavigationLinks constant for header navigation

---
+++ src/constants/navigation-items.ts
@@ -0,0 +1,40 @@
+import { HeaderNavLink } from '../enums/navigation';
+
+export const NavigationLinks: HeaderNavLink[] = [
+  {
+    label: 'Home',
+    href: '/',
+    slugRef: '',
+    title: 'Home'
+  },
+  {
+    label: 'Discover',
+    href: '/discover',
+    slugRef: '',
+    title: 'Discover'
+  },
+  {
+    label: 'Profile',
+    href: '/profile',
+    slugRef: '',
+    title: 'Profile'
+  },
+  {
+    label: 'Sell',
+    href: '/sell',
+    slugRef: '',
+    title: 'Sell'
+  },
+  {
+    label: 'Stats',
+    href: '/stats',
+    slugRef: '',
+    title: 'Stats'
+  },
+  {
+    label: 'Creators',
+    href: '/creators',
+    slugRef: '',
+    title: 'Creators'
+  }
+];
